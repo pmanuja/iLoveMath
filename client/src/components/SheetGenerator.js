13d2ee8eff7dd4f0f3fae975cd1117455379050a
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-06-15 16:43:50 -0400

    added css to  sheet

--- client/src/components/SheetGenerator.js
+++ client/src/components/SheetGenerator.js
@@ -3,14 +3,15 @@ import React, {Component} from 'react';
 class SheetGenerator extends Component {
   render(){
     return (
-      <div>{this.props.data.map((element, index) => {
+      <div className = "grid-container">{this.props.data.map((element, index) => {
         if(element[0] < element[1]){
           return (
               <div className = "qTable" key = {index}>
               <table>
                 <tbody>
-                  <tr><td>{index + 1})</td><td>{element[1]}</td></tr>
-                  <tr><td></td><td className="lastRow">{this.props.operation}{element[0]}</td></tr>
+                  <tr><td>{index + 1})</td><td></td><td>{element[1]}</td></tr>
+                  <tr><td></td><td>{this.props.operation}</td><td>{element[0]}</td></tr>
+                  <hr className="lastRow"/>
                 </tbody>
               </table>
               <br/>
@@ -23,8 +24,9 @@ class SheetGenerator extends Component {
               <div className = "qTable" key = {index}>
               <table>
                 <tbody>
-                  <tr><td>{index + 1})</td><td>{element[0]}</td></tr>
-                  <tr><td></td><td className="lastRow">{this.props.operation}{element[1]}</td></tr>
+                  <tr><td>{index + 1})</td><td></td><td>{element[0]}</td></tr>
+                  <tr><td></td><td>{this.props.operation}</td><td>{element[1]}</td></tr>
+                  <hr className="lastRow"/>
                 </tbody>
               </table>
               <br/>
